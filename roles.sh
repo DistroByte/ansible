cfcddb494f5e99c4432d93464a79c7dcc00c7f58
DistroByte <jamesthackett1@gmail.com>
2022-08-23 02:11:26 +0100

    adds links to readme

--- roles.sh
+++ roles.sh
@@ -8,7 +8,7 @@ grep -Ev '\|.[a-z0-9-]+.\|.[a-zA-Z].*\|' README.md > out$num
 for dir in $(find ./* -maxdepth 0 -type d -not -path "*/group_vars" -not -path "*/.git")
 do
   desc=$(sed '3q;d' ${dir}/README.md)
-  tablerow="| ${dir:2} | ${desc%?} |"
+  tablerow="| [${dir:2}](./${dir:2}) | ${desc%?} |"
   
   { head -n $num out$num; echo $tablerow; tail -n +$(($num + 1)) out$num; } > out$(($num + 1))
 
